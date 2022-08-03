4a82d7637f9609ce5bd6774349fde06e5be1ba22
CRYS <mariano.christopher123@gmail.com>
2022-08-03 15:21:12 +0800

    edit App.css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import NavBar from "./components/NavBar"
 
 function App() {
     const [accounts, setAccounts] = useState([])
+
     return (
-        <div className="App">
-            <NavBar accounts={accounts} setAccounts={setAccounts} />
-            <MainMint accounts={accounts} setAccounts={setAccounts} />
+        <div className="overlay">
+            <div className="App">
+                <NavBar accounts={accounts} setAccounts={setAccounts} />
+                <MainMint accounts={accounts} setAccounts={setAccounts} />
+            </div>
+            <div className="moving-background"></div>
         </div>
     )
 }
