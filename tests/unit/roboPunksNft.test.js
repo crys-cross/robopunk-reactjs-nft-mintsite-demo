2c2228151edea62ecc24109cf0b479ea0d67c7c3
CRYS <mariano.christopher123@gmail.com>
2022-09-20 20:10:05 +0800

    update unit test script

--- tests/unit/roboPunksNft.test.js
+++ tests/unit/roboPunksNft.test.js
@@ -4,30 +4,33 @@ const developmentChains = ["hardhat", "localhost"]
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("RoboPunksNFT Unit Test", () => {
-          let roboPunksNft, deployer
+          let roboPunksNft, deployer, otherWallet
 
           beforeEach(async () => {
               accounts = await ethers.getSigners()
               deployer = accounts[0]
+              otherWallet = accounts[0]
               await deployments.fixture(["RoboPunksNFT"])
               roboPunksNft = await ethers.getContract("RoboPunksNFT")
           })
           describe("constructor", async () => {
-              it("sets", (async) => {})
+              it("sets starting values correctly", async () => {})
           })
           describe("setIsPublicMintEnabled", async () => {
-              it("sets", (async) => {})
+              it("sets", async () => {})
           })
           describe("setBaseTokenUri", async () => {
-              it("sets", (async) => {})
+              it("sets", async () => {})
           })
           describe("tokenURI", async () => {
-              it("sets", (async) => {})
+              it("sets", async () => {})
           })
           describe("withdraw", async () => {
-              it("sets", (async) => {})
+              it("only withdraw wallet may withdraw", async () => {
+                  //roboPunkawait
+              })
           })
           describe("mint", async () => {
-              it("sets", (async) => {})
+              it("sets", async () => {})
           })
       })
